2210184c0e77eb766fc5b53ed06d016042fe3874
igorlogius <67047467+igorlogius@users.noreply.github.com>
2025-05-27 14:12:19 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+[![](https://raw.githubusercontent.com/igorlogius/igorlogius/main/geFxAddon.png)](https://addons.mozilla.org/firefox/addon/group-same-container-tabs/)
+
+[Report a bug, make a suggestion or ask a question](https://github.com/igorlogius/igorlogius/issues/new/choose)
+
+https://github.com/user-attachments/assets/9975c357-a5aa-4609-a676-b3988c641046
---
+++ background.js
@@ -0,0 +1,72 @@
+/* global browser */
+
+async function grpTabsByCookieStore(all_tabs, cookieStoreIds) {
+  cookieStoreIds.forEach((cookieStoreId) => {
+    const tabIds = all_tabs
+      .filter((t) => t.cookieStoreId === cookieStoreId)
+      .map((t) => t.id);
+    browser.tabs.group({ tabIds });
+  });
+}
+
+async function grpAllTabsByCookieStore() {
+  // 1. get all tabs
+  const all_tabs = await browser.tabs.query({
+    currentWindow: true,
+    pinned: false,
+    hidden: false,
+  });
+  const cookieStoreIds_tabIds = new Map(); // str => set(ints)
+  all_tabs.forEach((t) => {
+    tmp = cookieStoreIds_tabIds.get(t.cookieStoreId);
+    if (!tmp) {
+      tmp = new Set();
+    }
+    tmp.add(t.id);
+    cookieStoreIds_tabIds.set(t.cookieStoreId, tmp);
+  });
+
+  // create the groups and move the tabs
+  for (const [k, v] of cookieStoreIds_tabIds) {
+    console.debug(k, v);
+    const grpId = await browser.tabs.group({
+      tabIds: [...v],
+    });
+
+    if (browser.tabGroups) {
+      browser.tabGroups.update(grpId, {
+        title: k,
+      });
+    }
+  }
+}
+
+browser.menus.create({
+  title: "This/These Tabs",
+  contexts: ["tab"],
+  onclick: async (clickdata, atab) => {
+    const all_tabs = await browser.tabs.query({
+      currentWindow: true,
+      pinned: false,
+      hidden: false,
+    });
+
+    if (!atab.highlighted) {
+      const atab_origin = new URL(atab.url).origin;
+      grpTabsByCookieStore(all_tabs, [atab_origin]);
+    } else {
+      const sites = new Set(
+        all_tabs.filter((t) => t.highlighted).map((t) => t.cookieStoreId),
+      );
+      grpTabsByCookieStore(all_tabs, [...sites]);
+    }
+  },
+});
+
+browser.menus.create({
+  title: "All Tabs",
+  contexts: ["tab"],
+  onclick: async (clickdata, tab) => {
+    grpAllTabsByCookieStore();
+  },
+});
